013f1b8f9308cc61b4f9d4c596880ab549b908cd
Artikunazo <artik16032011@gmail.com>
2022-05-04 15:34:48 -0700

    [FT] Add meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { ResumeService } from '@core/services/resume/resume.service';
 
 @Component({
@@ -8,8 +9,18 @@ import { ResumeService } from '@core/services/resume/resume.service';
 })
 export class AppComponent {
   constructor(
-    private _resumeService: ResumeService
+    private _resumeService: ResumeService,
+    private _meta: Meta,
+    private _title: Title
   ) {
     this._resumeService.getResume();
+
+    // Set Meta Tags
+    this._meta.addTags([
+      { name: 'author', content: 'Arturo Casas' },
+      { name: 'description', content: 'Arturo Casas - Software Engineer' },
+      { name: 'keywords', content: ''}
+    ]);
+    this._title.setTitle('Arturo Casas - Software Engineer');
   }
 }
