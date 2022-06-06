6f52c94bc9edcb3602d0dd11e6e09186521df7dd
Artikunazo <artik16032011@gmail.com>
2022-06-06 11:31:32 -0700

    [FIX] GAId for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   api_url: 'https://resume-api-344305.uc.r.appspot.com/',
-  api_version: 'v1/'
+  api_version: 'v1/',
+  gaId: 'G-C6R09CDCM7'
   //resume/all
 };
