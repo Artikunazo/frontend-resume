0b48b0ced84330f553b06f23532b855211165d47
Arturo Casas <artik16032011@gmail.com>
2022-07-16 01:56:01 -0700

    [IMP] Change API URLs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,12 @@
 
 export const environment = {
   production: false,
-  api_url: 'https://get-resume-vdtctmrxwa-uc.a.run.app/',
+  //api_url: 'https://get-resume-vdtctmrxwa-uc.a.run.app/',
+  api_url: 'https://api-resume.vercel.app/v1/resume/all',
   api_version: 'v1/',
   gaId: 'G-C6R09CDCM7',
-  contact_api: 'https://contact-vdtctmrxwa-uc.a.run.app'
+  //contact_api: 'https://contact-vdtctmrxwa-uc.a.run.app'
+  contact_api: 'https://api-resume.vercel.app/v1/resume/contact'
   //resume/all
 };
 
