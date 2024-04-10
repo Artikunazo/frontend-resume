85e9cd11c545dd5da720f0e3a928fb66a15fbdca
Artikunazo <artik16032011@gmail.com>
2024-04-10 00:10:27 -0600

    [IMP] Some improves

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,39 @@
-import { Component } from '@angular/core';
-import { Meta, Title } from '@angular/platform-browser';
-import { ResumeService } from '@app/api/resume.service';
-import { environment } from '@environments/environment';
+import {Component} from '@angular/core';
+import {Meta, Title} from '@angular/platform-browser';
+import {ResumeService} from '@app/api/resume.service';
+import {environment} from '@environments/environment';
 
 declare let gtag: Function;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+	constructor(
+		private _resumeService: ResumeService,
+		private _meta: Meta,
+		private _title: Title,
+	) {
+		this._resumeService.getResume();
 
+		// Set Meta Tags
+		this._meta.addTags([
+			{name: 'author', content: 'Arturo Casas'},
+			{name: 'description', content: 'Frontend Engineer'},
+			{
+				name: 'keywords',
+				content:
+					'web software engineer, frontend developer, angular Developer, javascript developer, frontend engineer, fullstack mean',
+			},
+		]);
+		this._title.setTitle('Arturo Casas | Web Software Engineer');
 
-  constructor(
-    private _resumeService: ResumeService,
-    private _meta: Meta,
-    private _title: Title
-  ) {
-    this._resumeService.getResume();
-
-    // Set Meta Tags
-    this._meta.addTags([
-      { name: 'author', content: 'Arturo Casas' },
-      { name: 'description', content: 'Web Software Engineer' },
-      {
-        name: 'keywords',
-        content: 'Web Software Engineer, Frontend Developer, Angular Developer, Javascript Developer',
-      },
-    ]);
-    this._title.setTitle('Arturo Casas | Web Software Engineer');
-
-    // GA
-    const request = new XMLHttpRequest();
-    gtag('Visit', environment.gaId, {
-      data: request.getAllResponseHeaders()
-    });
-  }
+		// GA
+		const request = new XMLHttpRequest();
+		gtag('Visit', environment.gaId, {
+			data: request.getAllResponseHeaders(),
+		});
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,27 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import {NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
 
 /* App */
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import {AppComponent} from './app.component';
 
 /* Core */
-import { CoreModule } from '@app/core/core.module'
+import {CoreModule} from '@app/core/core.module';
 
 /* Container */
-import { ContainerModule } from '@app/container/container.module';
-
+import {ContainerModule} from '@app/container/container.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    ContainerModule,
-    CoreModule
-  ],
-  bootstrap: [AppComponent],
-  schemas: [
-    CUSTOM_ELEMENTS_SCHEMA
-  ]
+	declarations: [AppComponent],
+	imports: [
+		BrowserModule,
+		HttpClientModule,
+		ReactiveFormsModule,
+		ContainerModule,
+		CoreModule,
+	],
+	bootstrap: [AppComponent],
+	schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
