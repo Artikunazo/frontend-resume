3ad69374bf6e7374e6e4dc2fe2b3daff2742178a
Artikunazo <artik16032011@gmail.com>
2024-04-13 11:21:41 -0600

    [FT] Add dashboard kanban project

--- src/app/tech-skills/tech-skills.component.ts
+++ src/app/tech-skills/tech-skills.component.ts
@@ -1,50 +1,46 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ResumeService } from '@app/api/resume.service';
-import { Subscription } from 'rxjs';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {ResumeService} from '@app/api/resume.service';
+import {Subscription} from 'rxjs';
 
 @Component({
-  selector: 'tech-skills',
-  templateUrl: './tech-skills.component.html',
-  styleUrls: ['./tech-skills.component.scss']
+	selector: 'tech-skills',
+	templateUrl: './tech-skills.component.html',
+	styleUrls: ['./tech-skills.component.scss'],
 })
 export class TechSkillsComponent implements OnInit, OnDestroy {
+	skills: string[] = [];
 
-  skills: string[] = [];
+	private _subs = new Subscription();
 
-  private _subs = new Subscription();
+	constructor(private _resumeService: ResumeService) {}
 
-  constructor(
-    private _resumeService: ResumeService
-  ) { }
+	ngOnInit(): void {
+		this._subs.add(
+			this._resumeService
+				.getInfoByCategory('techSkills')
+				.subscribe((skills) => {
+					this.skills = skills.sort();
+				}),
+		);
+	}
 
-  ngOnInit(): void {
-    this._subs.add(
-      this._resumeService
-      .getInfoByCategory('techSkills')
-      .subscribe((skills) => {
-        this.skills = skills.sort();
-      })
-    );
-  }
+	findIconSkill(skill: string): string {
+		skill = skill.toLowerCase().replace(/\s/g, '-');
 
-  findIconSkill(skill: string): string {
-    skill = skill.toLowerCase().replace(/\s/g, '-');
+		if (skill.includes('gary-css')) {
+			return '/assets/images/icons/open-source-icon.svg';
+		}
 
-    if(skill.includes('gary-css')){
-      return '/assets/images/icons/open-source-icon.svg';
-    }
+		let icon = '/assets/images/icons/' + skill + '-icon.svg';
 
-    let icon = '/assets/images/icons/' + skill + '-icon.svg';
+		if (!icon) {
+			icon = '/assets/images/icons/default.svg';
+		}
 
-    if(!icon){
-      icon = '/assets/images/icons/default.svg';
-    }
+		return icon;
+	}
 
-    return icon;
-  }
-
-  ngOnDestroy(): void {
-    this._subs.unsubscribe();
-  }
-
+	ngOnDestroy(): void {
+		this._subs.unsubscribe();
+	}
 }
